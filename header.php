52a4c35b6ded2eaaf8e32ded93f8eb6f5a0f91ac
ElenaOrozco <elena.orozcoch@gmail.com>
2017-05-06 19:26:59 -0500

    Primer commit. Maquetacion y navbar version 1

---
+++ header.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Orozco Rines</title>
+	<!-- Required meta tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Prompt|Ranga:700" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos_rines.css">
+  </head>
+  <body>
+</head>
+<body>
+	<!-- Header Nav -->
+	<header>
+		<nav class="navbar navbar-toggleable-md navbar-light fixed-top bg-faded" style="background: #000;">
+		  <button class="navbar-toggler navbar-toggler-right mr-2" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+		    <i class="fa fa-bars" aria-hidden="true"></i>
+		  </button>
+		  <a class="navbar-brand text-uppercase" href="#"><img class="px-1" src="img/tire.svg" alt="">rozco Rines</a>
+		  <div class="texto text-center">
+		  	
+		  	<p class="h4"><img class= "px-3" src="img/phone-call.svg" alt="">3310186473</p>
+		  </div>
+		  <div class="menus">
+		  	<div class="login">
+		  		<a href="" class="px-md-3 text-capitalize">Login</a>
+		  		<a href="" class="px-md-3 text-capitalize">Status Pedido</a>
+		  	</div>
+		  	<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+			    <ul class="navbar-nav mt-2 mt-md-0">
+			      <li class="nav-item active  px-md-3">
+			        <a class="nav-link text-uppercase" href="#">Home <span class="sr-only">(current)</span></a>
+			      </li>
+			      <li class="nav-item px-md-3">
+			        <a class="nav-link text-uppercase" href="#">Rines</a>
+			      </li>
+			      <li class="nav-item px-md-3">
+			        <a class="nav-link text-uppercase" href="#">Llantas</a>
+			      </li>
+			      <li class="nav-item px-md-3">
+			        <a class="nav-link text-uppercase" href="#">Contacto</a>
+			      </li>
+			    </ul>
+			    <!--<i class="fa fa-search" aria-hidden="true"></i>-->
+			  </div>
+		  </div>
+			
+		  
+		</nav><!-- /nav-->
+
+		<!-- contenedor-->
+		<div class="container-fluid row mx-md-0 px-0 px-md-0">
+			<!-- carrousel-->
+			<div id="carouselExampleIndicators" class="carousel slide col-md-8 px-md-0" data-ride="carousel">
+			  <ol class="carousel-indicators">
+			    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			  </ol>
+			  <div class="carousel-inner" role="listbox">
+			    <div class="carousel-item active">
+			      <img class="d-block img-fluid" src="img/2.jpg" alt="First slide">
+			      <div class="carousel-caption d-none d-md-block">
+					    <h3>Rin1</h3>
+					    <p>Loem	inpsump</p>
+					  </div>
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/1.jpg" alt="Second slide">
+			      <div class="carousel-caption d-none d-md-block">
+					    <h3>Rin2</h3>
+					    <p>Loem	inpsump</p>
+					  </div>
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block img-fluid" src="img/3.jpg" alt="Third slide">
+			      <div class="carousel-caption d-none d-md-block">
+					    <h3>Rin3</h3>
+					    <p>Loem	inpsump</p>
+					  </div>
+			    </div>
+			  </div>
+			  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
+			<div class="promo col-md-4 px-md-0" style="height:100vh; background: #ff3d00;">
+				
+
+			</div>
+		</div>
+
+		
+	</header>
+	
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php 
+require 'header.php';
+?>
+
+<!-- Productos -->
+<div class="productos" style="background: #000">
+	<div class="categoria" style="background: blue">
+		
+	</div>
+	<div class="galeria-productos" style="background: pink">
+		<div class="item-producto" style="background: tomato"></div>
+		<div class="item-producto" style="background: tomato"></div>
+		<div class="item-producto" style="background: tomato"></div>
+		<div class="item-producto" style="background: tomato"></div>
+	</div>
+</div>
+<div class="productos" style="background: #000">
+	<div class="categoria" style="background: blue">
+		
+	</div>
+	<div class="galeria-productos" style="background: pink">
+		<div class="item-producto" style="background: tomato"></div>
+		<div class="item-producto" style="background: tomato"></div>
+		<div class="item-producto" style="background: tomato"></div>
+		<div class="item-producto" style="background: tomato"></div>
+	</div>
+</div>
+
+
+<!-- Ver Mas -->
+<div class="ver-mas">
+	
+</div>
+<!-- Proceso Compra -->
+<div class="proceso-compra">
+	<h3>Lorem insup</h3>
+	<div class="pasos">
+		<div class="paso">
+			<a href="">icono</a>
+			<p>descripcion</p>
+		</div>
+		<div class="paso">
+			
+		</div>
+		<div class="paso">
+			
+		</div>
+	</div>
+</div>
+
+<!-- Clientes -->
+<div class="clientes">
+	
+</div>
+
+<div class="mas-vistos">
+	<div class="categoria" style="background: blue">
+		
+	</div>
+	<div class="galeria-productos" style="background: pink">
+		<div class="item-producto" style="background: tomato"></div>
+		<div class="item-producto" style="background: tomato"></div>
+		<div class="item-producto" style="background: tomato"></div>
+		<div class="item-producto" style="background: tomato"></div>
+	</div>
+</div>
+
+<div class="marcas">
+	
+</div>
+<div class="tel">
+	
+</div>
+<?php require 'footer.php'; ?>
+
